fe53893564819e87cfda72ebadd509c64717134f
Chris <chris.tsimogiannis@rmb.co.za>
2018-03-13 10:08:00 +0200

    refactored errors

--- src/js/components/Web3DetailsExample.js
+++ src/js/components/Web3DetailsExample.js
@@ -6,24 +6,25 @@ import { notifyBlockNumberChanged, notifyAccountChanged } from "../util/web3Util
 export default class Web3DetailsExample extends React.Component {
 
     render() {
-        var body = (this.props.web3.loading) ? 
-        (<h1>Loading</h1>) : 
-        (
+        var web3 = this.props.web3;
+
+        if(!web3) {
+            return <div>Web3 not installed.</div>
+        } else if(web3.loading) {
+            return (<div>Loading</div>);
+        } else if (web3.error) {
+            return (<div>Unable to get web3: {web3.error.toString()}</div>)
+        } else {
+            return (
             <div>
                 <h1>Web3 {this.props.web3.version}</h1>
                 <div>{this.props.web3.error}</div>
 
                 <div><NetworkDetails networkDetails={this.props.networkDetails }/></div>
                 <div><AccountDetails accountDetails={this.props.accountDetails }/></div>
-            </div>
-        );
-
-        return (    
-            <div>
-                {body}
-            </div>
-        );
-    }
+            </div>)
+        }
+    }    
 }
 
 const NetworkDetails = (props) => {
@@ -34,7 +35,7 @@ const NetworkDetails = (props) => {
     } else if(networkDetails.loading) {
         return (<div>Loading</div>);
     } else if (networkDetails.error) {
-        return (<div>Unable to get network details: {networkDetails.error}</div>)
+        return (<div>Unable to get network details: {networkDetails.error.toString()}</div>)
     } else {
         return (
         <div>
@@ -43,7 +44,6 @@ const NetworkDetails = (props) => {
             <div>Is Syncing: {networkDetails.isSyncing ? networkDetails.isSyncing : "false"}</div>
         </div>)
     }
-
 }
 
 const AccountDetails = (props) => {
@@ -54,7 +54,7 @@ const AccountDetails = (props) => {
     } else if(accountDetails.loading) {
         return (<div>Loading</div>);
     } else if (accountDetails.error) {
-        return (<div>Unable to get account details: {accountDetails.error}</div>)
+        return (<div>Unable to get account details: {accountDetails.error.toString()}</div>)
     } else if (!accountDetails.accounts || accountDetails.accounts.length == 0) {
         return <div>No account selecteds.</div>
     } else {
--- src/js/reducers/web3Reducer.js
+++ src/js/reducers/web3Reducer.js
@@ -13,21 +13,21 @@ export default function reducer(state = initialState, action) {
         case "GET_WEB3_FULFILLED":
             return {...state, web3: { ...action.payload, loading: false, error: null}};
         case "GET_WEB3_REJECTED":
-            return {...state, web3: { loading: false, error: action.payload.toString() }};
+            return {...state, web3: { loading: false, error: action.payload }};
 
         case "GET_WEB3_NETWORK_DETAILS_PENDING":
             return {...state, networkDetails: { loading: true } };
         case "GET_WEB3_NETWORK_DETAILS_FULFILLED":
             return {...state, networkDetails: { ...action.payload, loading: false, error: null}};
         case "GET_WEB3_NETWORK_DETAILS_REJECTED":
-            return {...state, networkDetails: { loading: false, error: action.payload.toString()}};
+            return {...state, networkDetails: { loading: false, error: action.payload}};
 
         case "GET_WEB3_ACCOUNT_DETAILS_PENDING":
             return {...state, accountDetails: { loading: true } };
         case "GET_WEB3_ACCOUNT_DETAILS_FULFILLED":
             return {...state, accountDetails: { ...action.payload, loading: false, error: null}};
         case "GET_WEB3_ACCOUNT_DETAILS_REJECTED":
-            return {...state, accountDetails: { loading: false, error: action.payload.toString()}};
+            return {...state, accountDetails: { loading: false, error: action.payload}};
 
         default:
             return state;
